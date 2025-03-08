48daf8d14c2a79ac2fe0c2dfdd03698bc70350c1
antonioVargas <vsavila.antonio@gmail.com>
2025-03-08 15:33:44 -0600

    React portfolio stted with Vite

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="bg-amber-500">
+    Hola
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
